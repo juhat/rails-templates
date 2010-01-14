ddd92be856bf4f498295a3c0a0709da1ec6482b9
Attila Juhász <juhasz@atti.la>
2010-01-14 02:20:49 +0100

    New revision

--- base.rb
+++ base.rb
@@ -3,20 +3,28 @@ if yes?("Do you want to use RSpec for testing?")
   plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
   generate :rspec
 end
- 
+
+run "gem install nifty-generators", :sudo => true
 generate :nifty_layout
- 
-git :init
- 
+
 run "echo 'TODO add readme content' > README"
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
 run "cp config/database.yml config/example_database.yml"
- 
+run "rm public/index.html"
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
+
 file ".gitignore", <<-END
 log/*.log
 tmp/**/*
 config/database.yml
 db/*.sqlite3
 END
- 
-git :add => ".", :commit => "-m 'initial commit'"
+
+git :init
+
+plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true
+rake 'asset:packager:create_yml'
+
+git :submodule => "init"
+git :add => ".", :commit => "-m 'Initial commit.'"
