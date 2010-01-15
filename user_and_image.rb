b5aeee9b6c9870008fadc09ad1a3493dcfe3b89c
Attila Juhász <juhasz@atti.la>
2010-01-15 19:47:46 +0100

    Bugfix.

--- user_and_image.rb
+++ user_and_image.rb
@@ -138,6 +138,10 @@ class ApplicationController < ActionController::Base
       end
     end
     
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+    
     def redirect_back_or_default(default)
         redirect_to(session[:return_to] || default)
         session[:return_to] = nil
