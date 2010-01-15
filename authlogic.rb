b53fdf67430c50d0f8af89905cbddb0e59757898
Attila Juhász <juhasz@atti.la>
2010-01-15 21:44:05 +0100

    Bugfix.

--- authlogic.rb
+++ authlogic.rb
@@ -301,12 +301,14 @@ END
 generate :mailer, 'UserMailer'
 
 file 'app/models/user_mailer', <<-END
-def welcome_email(user, pwd)
-  recipients    user.email
-  from          "Test User <test@atti.la>"
-  subject       "Registration"
-  sent_on       Time.now
-  body          {:user => user, :password => pwd}
+class UserMailer < ActionMailer::Base
+  def welcome_email(user, pwd)
+    recipients    user.email
+    from          "Test User <test@atti.la>"
+    subject       "Registration"
+    sent_on       Time.now
+    body          {:user => user, :password => pwd}
+  end
 end
 END
 
